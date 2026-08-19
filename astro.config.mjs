8a8b723c4deadcbe8cc802c5c372acc49ebc4fc9
Satej Bidvai <satej.bidvai@gmail.com>
2026-08-20 01:50:45 +0530

    sounds are fun!

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,7 @@ import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
 import tailwindcss from '@tailwindcss/vite';
+
 // https://astro.build/config
 export default defineConfig({
   site: 'https://satejbidvai.com',
